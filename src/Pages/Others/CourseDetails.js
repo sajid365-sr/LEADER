4bbf10b8237b38eda15bc83c98c5b5e5cc0109fb
sajid365-sr <sajidsorker86@gmail.com>
2022-11-03 21:39:59 +0600

    course details container setup

--- src/Pages/Others/CourseDetails.js
+++ src/Pages/Others/CourseDetails.js
@@ -1,15 +1,20 @@
 
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import './CourseDetails.css';
+
 
 const CourseDetails = () => {
-    const cDetails = useLoaderData();
-    console.log(cDetails)
-    return (
-        <div>
-            <h2>This is Course Details page</h2>
-        </div>
-    );
+  const { topic, details, enrolled, image, instructor, price, ratings, id } =
+    useLoaderData();
+  console.log(image)
+  return (
+    <div className="mx-auto bgImage" style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.9)0%, rgba(0, 0, 0, 0.2)100%), url(${image})` }}>
+
+      <h2 className="text-center py-3 mb-0 text-decoration-underline">{topic}</h2>
+      
+    </div>
+  );
 };
 
 export default CourseDetails;
